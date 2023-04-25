b9d57ededb2145cb9904fc760a44691a945e1b56
epieffe.eth <ferrariepifanio@gmail.com>
2023-04-26 00:21:38 +0200

    Updated dependencies and code refactor

--- index.js
+++ index.js
@@ -7,55 +7,11 @@ const PUBLIC_KEY = "0x726cDa2Ac26CeE89F645e55b78167203cAE5410E";
 const PRIVATE_KEY = "0x68619b8adb206de04f676007b2437f99ff6129b672495a6951499c6c56bc2fa6";
 // get current account nonce from OpenSea Shared Contract
 const NONCE = 0;
-
 // Account to transfer the nft to
 const RECEIVER = "0xbB072aF5D54BeCb477dffb4cCbdFE75cf1ceBFDB";
 
 const OPENSEA_CONTRACT = "0x2953399124f0cbb46d2cbacd8a89cf0599974963";
 const TOKEN_ID = "10110860822564241239994147652924744222037427536707093556420917701357093257416";
-
-// Opensea Shared Contract on Polygon
-// https://polygonscan.com/address/0x2953399124f0cbb46d2cbacd8a89cf0599974963
-const domainData = {
-    name: "OpenSea Collections",
-    version: "1",
-    verifyingContract: OPENSEA_CONTRACT,
-    chainId: 137
-};
-
-const domainType = [
-    {
-        name: "name",
-        type: "string"
-    },
-    {
-        name: "version",
-        type: "string"
-    },
-    {
-        name: "chainId",
-        type: "uint256"
-    },
-    {
-        name: "verifyingContract",
-        type: "address"
-    }
-];
-
-const metaTransactionType = [
-    {
-        name: "nonce",
-        type: "uint256"
-    },
-    {
-        name: "from",
-        type: "address"
-    },
-    {
-        name: "functionSignature",
-        type: "bytes"
-    }
-];
 
 const safeTransferFromAbi = {
     'inputs': [
@@ -92,15 +48,49 @@ const safeTransferFromAbi = {
 };
 
 const params = [PUBLIC_KEY, RECEIVER, TOKEN_ID, 1, '0x'];
-
 const functionSignature = web3Abi.encodeFunctionCall(safeTransferFromAbi, params);
 
 const dataToSign = {
     types: {
-        EIP712Domain: domainType,
-        MetaTransaction: metaTransactionType
+        EIP712Domain: [
+            {
+                name: "name",
+                type: "string"
+            },
+            {
+                name: "version",
+                type: "string"
+            },
+            {
+                name: "chainId",
+                type: "uint256"
+            },
+            {
+                name: "verifyingContract",
+                type: "address"
+            }
+        ],
+        MetaTransaction: [
+            {
+                name: "nonce",
+                type: "uint256"
+            },
+            {
+                name: "from",
+                type: "address"
+            },
+            {
+                name: "functionSignature",
+                type: "bytes"
+            }
+        ]
     },
-    domain: domainData,
+    domain: {
+        name: "OpenSea Collections",
+        version: "1",
+        chainId: 137,
+        verifyingContract: OPENSEA_CONTRACT
+    },
     primaryType: "MetaTransaction",
     message: {
         nonce: parseInt(NONCE),
