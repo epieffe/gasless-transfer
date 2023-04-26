f613a76fa8222a992f6fe6d987e6264e3776642f
epieffe.eth <ferrariepifanio@gmail.com>
2023-04-26 19:50:46 +0200

    Support for ERC1155 batch transfer

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const NONCE = 0;
 const RECEIVER = "0xbB072aF5D54BeCb477dffb4cCbdFE75cf1ceBFDB";
 
 const OPENSEA_CONTRACT = "0x2953399124f0cbb46d2cbacd8a89cf0599974963";
-const TOKEN_ID = "10110860822564241239994147652924744222037427536707093556420917701357093257416";
+
+const TOKEN_IDS = [
+    "27586265456489730858872113696342466140246807895101263013680124710712147705876",
+    "31443824730881887699422569955567000458264599455970345663847405214989499236353"
+];
+
+const BALANCES = [1, 1];
 
 const safeTransferFromAbi = {
     'inputs': [
@@ -48,11 +54,48 @@ const safeTransferFromAbi = {
     'type': "function"
 };
 
+const safeBatchTransferFromAbi = {
+    'inputs': [
+        {
+            'internalType': "address",
+            'name': "_from",
+            'type': "address"
+        },
+        {
+            'internalType': "address",
+            'name': "_to",
+            'type': "address"
+        },
+        {
+            'internalType': "uint256[]",
+            'name': "_ids",
+            'type': "uint256[]"
+        },
+        {
+            'internalType': "uint256[]",
+            'name': "_amounts",
+            'type': "uint256[]"
+        },
+        {
+            'internalType': "bytes",
+            'name': "_data",
+            'type': "bytes"
+        }
+    ],
+    'name': "safeBatchTransferFrom",
+    'outputs': [],
+    'stateMutability': "nonpayable",
+    'type': "function"
+}
+
 const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
 const publicKey = account.address;
 
-const params = [publicKey, RECEIVER, TOKEN_ID, 1, '0x'];
-const functionSignature = web3Abi.encodeFunctionCall(safeTransferFromAbi, params);
+//const params = [publicKey, RECEIVER, TOKEN_ID, 1, '0x'];
+//const functionSignature = web3Abi.encodeFunctionCall(safeTransferFromAbi, params);
+
+const params = [publicKey, RECEIVER, TOKEN_IDS, BALANCES, '0x'];
+const functionSignature = web3Abi.encodeFunctionCall(safeBatchTransferFromAbi, params);
 
 const dataToSign = {
     types: {
@@ -93,7 +136,7 @@ const dataToSign = {
         name: "OpenSea Collections",
         version: "1",
         verifyingContract: OPENSEA_CONTRACT,
-        salt: "0x0000000000000000000000000000000000000000000000000000000000000089"// 137 to bytes32
+        salt: "0x0000000000000000000000000000000000000000000000000000000000000089"// 137 (Polygon chainId) to bytes32
     },
     primaryType: "MetaTransaction",
     message: {
